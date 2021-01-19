008b105f64e9cee616baa17a8ea81ba1fc912ace
mierzwid <mierzwid@gmail.com>
2021-01-19 11:59:55 +0100

    restoring backups from AWS S3

--- 2019/publish-backup-to-s3.sh
+++ 2019/publish-backup-to-s3.sh
@@ -20,15 +20,20 @@ if [ -z "$AWS_S3_BUCKET_NAME" ]; then
   exit 1
 fi
 
+if [ -z "$AWS_S3_SSE_CUSTOMER_KEY" ]; then
+  echo "AWS-S3: You have to pass SSE Customer Key for client side encryption as 'AWS_S3_SSE_CUSTOMER_KEY' variable."
+  exit 1
+fi
+
 echo "AWS-S3: Publishing backup $AWS_S3_BACKUP_FILE_PATH to s3://$AWS_S3_BUCKET_NAME"
 
-aws s3 cp "$AWS_S3_BACKUP_FILE_PATH" "s3://$AWS_S3_BUCKET_NAME"
+echo "aws s3 cp --sse-c AES256 --sse-c-key fileb://$AWS_S3_SSE_CUSTOMER_KEY $AWS_S3_BACKUP_FILE_PATH s3://$AWS_S3_BUCKET_NAME"
+aws s3 cp --sse-c AES256 --sse-c-key "fileb://$AWS_S3_SSE_CUSTOMER_KEY" "$AWS_S3_BACKUP_FILE_PATH" "s3://$AWS_S3_BUCKET_NAME"
 PUBLICATION_RESULT=$?
 
 echo
 echo "AWS-S3: List of all published backups at s3://$AWS_S3_BUCKET_NAME"
 aws s3 ls "s3://$AWS_S3_BUCKET_NAME"
-
 
 echo
 if [ $PUBLICATION_RESULT -eq 0 ]; then
