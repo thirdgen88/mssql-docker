515063399e5bc78e112f064c0e1852a9ee6d7aac
mierzwid <mierzwid@gmail.com>
2021-01-21 13:49:26 +0100

    configuring backups schedule

--- backups/entrypoint.sh
+++ backups/entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Start the run once job.
-echo "DB-BACKUPS: Docker container has been started"
+CRON_SCHEDULE=${BACKUPS_CRON_SCHEDULE:-"0 0 * * *"}
+
+echo "DB-BACKUPS: Scheduling periodic backups: $CRON_SCHEDULE"
 
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 
 # Setup a cron schedule
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
-* * * * * /backup-daily.sh >> /var/log/cron.log 2>&1
+$CRON_SCHEDULE /backup-periodically.sh >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
 touch /var/log/cron.log
